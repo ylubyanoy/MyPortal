43bad2bf123ea56a8b62ebba0f4f4e3d3b08f507
ylubyanoy <dreif8270@gmail.com>
2017-05-02 22:46:13 +0300

    Settings for admin site

--- BlogMySelf/admin.py
+++ BlogMySelf/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from guestbook.models import Guestbook
 
 
-admin.site.register(Guestbook)
+class GuestbookAdmin(admin.ModelAdmin):
+    list_display = ("posted", "user", "content")
+    list_display_links = ("posted", "user")
+    list_per_page = 10
+    list_max_show_all = 50
+
+admin.site.register(Guestbook, GuestbookAdmin)
--- guestbook/models.py
+++ guestbook/models.py
@@ -6,6 +6,12 @@ class Guestbook(models.Model):
     posted = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
     content = models.TextField(verbose_name="Содержимое")
 
+    def get_formatted_datetime(self):
+        return str(self.posted.day) + "." + str(self.posted.month) + "." + str(self.posted.year) + " " + \
+               str(self.posted.hour) + ":" + str(self.posted.minute) + ":" + str(self.posted.second)
+
+    get_formatted_datetime.short_description = "Опубликовано"
+
     class Meta:
         ordering = ["-posted"]
         verbose_name = "запись гостевой книги"
