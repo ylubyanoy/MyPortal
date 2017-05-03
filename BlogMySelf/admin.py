3e2b669af363ca6455883ecf03759bee0208a45a
ylubyanoy <dreif8270@gmail.com>
2017-05-03 11:56:33 +0300

    Add another settings for admin site

--- BlogMySelf/admin.py
+++ BlogMySelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from guestbook.models import Guestbook
+from goods.models import Good
+from categories.models import Category
 
 
 class GuestbookAdmin(admin.ModelAdmin):
@@ -7,5 +9,47 @@ class GuestbookAdmin(admin.ModelAdmin):
     list_display_links = ("posted", "user")
     list_per_page = 10
     list_max_show_all = 50
+    search_fields = ("user", "content")
+    date_hierarchy = "posted"
+    list_filter = ("user",)
+    preserve_filters = False
+    ordering = ("user", "posted")
+
+
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    # readonly_fields = ("category",)
+    radio_fields = {"category": admin.VERTICAL}
+    save_on_top = True
+    fieldsets = (
+        (None, {
+            "fields": (("name", "category"),)
+        }),
+        ("Описание", {
+            "fields": ("description", "content")
+        }),
+        ("Цена", {
+            "fields": (("price", "price_acc"),)
+        }),
+        ("Дополнительные параметры", {
+            "classes": ("collapse",),
+            "fields": (("in_stock", "featured"),)
+        }),
+        ("Изображение", {
+            "fields": ("image",)
+        })
+    )
+
+
+class GoodInline(admin.StackedInline):
+    model = Good
+    fields = (("name", "category"), "description", "content", ("price", "price_acc"), ("in_stock", "featured"), "image")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = (("name", "order"),)
+    # inlines = (GoodInline,)
 
 admin.site.register(Guestbook, GuestbookAdmin)
+admin.site.register(Good, GoodAdmin)
+admin.site.register(Category, CategoryAdmin)
